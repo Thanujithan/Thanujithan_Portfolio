f7f1340df0903b67969685b34f500aaa6b715981
Thanujithan <thanuthanujithan@gmail.com>
2026-07-27 22:26:26 +0530

    Fix Admin model import

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-const Admin = require("../models/admin");
+const Admin = require("../models/Admin");
 
 const adminAuth = async (req, res, next) => {
     try {
